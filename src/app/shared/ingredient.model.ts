fa70e2746f3f29871de6bdd1c400031b639242a8
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-07-31 17:20:17 +0200

    [done] Section 3: Course Project - The Basics | 56. Creating an "Ingredient" Model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,19 @@
+export class Ingredient {
+
+  /* // First approach but shorter in the second approach
+  * public name: string;
+  * public amount: number;
+
+  * constructor(name: string, amount: number) {
+  *   this.name = name;
+  *   this.amount = amount;
+  * }
+  */
+
+  /* // Second approach
+  * constructor(public name: string, public amount: number) {}
+  */
+
+  constructor(public name: string, public amount: number) {}
+
+}
