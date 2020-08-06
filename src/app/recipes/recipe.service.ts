b18b75684ec908c982d6ab1eddf85508c3b3c95b
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-08-06 20:46:34 +0200

    [done] Section 10: Course Project - Services & Dependency Injection | 122. Passing Ingredients from Recipes to the Shopping List (via a Service)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,40 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test (maybe)', 'https://img-global.cpcdn.com/recipes/5559831272357888/640x640sq70/photo.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test (maybe)', 'https://recipecontent.fooby.ch/15287_3-2_480-320.jpg')
+    new Recipe(
+      'Martabak',
+      'Ein Biss in die süße Kindheit!',
+      'https://img-global.cpcdn.com/recipes/5559831272357888/640x640sq70/photo.jpg',
+      [
+        new Ingredient('Flour', 1),
+        new Ingredient('Nutella', 5)
+      ]),
+    new Recipe(
+      'Fake Gado-Gado',
+      'Wenn du lediglich "versuchen" möchtest, es richtig zu machen.',
+      'https://recipecontent.fooby.ch/15287_3-2_480-320.jpg',
+      [
+        new Ingredient('Gado 1', 2),
+        new Ingredient('Gado 2', 3)
+      ])
   ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
   }
 
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
