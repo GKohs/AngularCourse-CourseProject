f7ac98d782b698ad804f64e0b708eda492e4cf90
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-08-26 18:14:14 +0200

    [done] Section 19: Course Project - Http | 281. Fetching Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
+  httpUrl: string =  'https://ng-course-project-2aa9a.firebaseio.com/recipes.json';
+
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    const httpUrl: string =  'https://ng-course-project-2aa9a.firebaseio.com/recipes.json';
     this.http
-      .put(httpUrl, recipes)
+      .put(this.httpUrl, recipes)
       .subscribe(response => {
         console.log(response);
       });
-
   }
 
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(this.httpUrl)
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+        console.log(recipes);
+      });
+  }
 }
