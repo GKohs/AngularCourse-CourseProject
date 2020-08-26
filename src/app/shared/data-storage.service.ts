a7e590f2ed534a4fd30ae916aa5c064991b49808
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-08-26 18:39:10 +0200

    [done] Section 19: Course Project - Http | 283. Resolving Data Before Loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,7 @@
 import { Ingredient } from './ingredient.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
@@ -22,16 +22,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(this.httpUrl)
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
         });
-      }))
-      .subscribe(recipes => {
+      }),
+      tap(recipes => {
         this.recipeService.setRecipes(recipes);
-        console.log(recipes);
-      });
+      })
+      );
   }
 }
