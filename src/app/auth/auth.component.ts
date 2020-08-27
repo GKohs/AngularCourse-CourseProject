3d6d8072c8adedbbee560c5872d4cb18df9ee2be
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-08-27 09:59:53 +0200

    [done] Section 20: Course Project - Authentication & Route Protection in Angular | 296. Sending Login Requests

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
-import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -25,19 +27,23 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode) {
-      // ...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(resData => {
-        console.log(resData);
-        this.isLoading = false;
-      }, errorMsg => {
-        console.log(errorMsg);
-        this.error = errorMsg;
-        this.isLoading = false;
-      });
+      authObs = this.authService.signup(email, password);
     }
+
+    authObs.subscribe(resData => {
+      console.log(resData);
+      this.isLoading = false;
+    }, errorMsg => {
+      console.log(errorMsg);
+      this.error = errorMsg;
+      this.isLoading = false;
+    });
 
     // console.log(form.value);
     // form.reset();
