78872a25c35a9ee9194791ab043909f284042acd
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-08-31 07:23:49 +0200

    [done] Section 22: Angular Modules & Optimizing Angular Apps | 327. Understanding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptiorService } from './auth/auth-interceptor.service';
 
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -29,16 +28,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptiorService,
-      multi: true
-    }
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
