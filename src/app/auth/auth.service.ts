dbe668f65bcebc3721091f2ec8ecd48b259b3ce0
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-09-05 11:01:37 +0200

    [done] Section 24: Bonus: Working with NgRx in our Project | 369. Finishing the Login Effect

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -84,7 +84,7 @@ export class AuthService {
 
     if (loadedUser.token) {
       // this.user.next(loadedUser);
-      this.store.dispatch(new AuthActions.Login({ email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate) }));
+      this.store.dispatch(new AuthActions.AuthenticateSuccess({ email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate) }));
       const expirationDuration =
         new Date(userData._tokenExpirationDate).getTime() -
         new Date().getTime();
@@ -123,7 +123,7 @@ export class AuthService {
       expirationDate
     );
     // this.user.next(user);
-    this.store.dispatch(new AuthActions.Login({ email: email, userId: userId, token: token, expirationDate: expirationDate }));
+    this.store.dispatch(new AuthActions.AuthenticateSuccess({ email: email, userId: userId, token: token, expirationDate: expirationDate }));
     this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
