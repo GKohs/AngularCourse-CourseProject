2baf3cf603a9e61a65b8f3eb14d6f7ed320e97fe
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-09-05 07:46:24 +0200

    [done] Section 24: Bonus: Working with NgRx in our Project | 367. Login via NgRx Effects WiP - with errors at login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { SharedModule } from './shared/shared.module';
 
 import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
+import { AuthEffects } from './auth/store/auth.effects';
 // import { LoggingService } from './logging.service';
 
 @NgModule({
@@ -22,6 +24,7 @@ import * as fromApp from './store/app.reducer';
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
     SharedModule,
     CoreModule
   ],
