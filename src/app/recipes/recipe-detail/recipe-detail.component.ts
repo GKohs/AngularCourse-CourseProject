b2754155b5099f9e7ed319c1214b7441f8dbd8fa
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-09-09 20:58:11 +0200

    [done] Section 24: Bonus: Working with NgRx in our Project | 379. Fetching Recipe Detail Data

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,19 +16,30 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute,
-              private router: Router ) { }
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<fromApp.AppState>
+  ) { }
 
   ngOnInit(): void {
     // Only one time solution, not preferable in this app
     // const id = this.route.snapshot.params['id'];
-    this.route.params.subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-          this.recipe = this.recipeService.getRecipe(this.id);
-        }
-    );
+    this.route.params.pipe(map(params => {
+      return +params['id'];
+    }), switchMap(id => {
+      this.id = id;
+      return this.store.select('recipes');
+    }),
+    map(recipeState => {
+      return recipeState.recipes.find((recipe, index) => {
+        return index === this.id;
+      });
+    }))
+    .subscribe(recipe => {
+      this.recipe = recipe;
+    });
   }
 
   onAddToShoppingList() {
