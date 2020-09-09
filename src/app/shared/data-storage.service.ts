e2768a5419ec59f67a9fab78866dd502eb247909
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-09-09 20:35:08 +0200

    [done] Section 24: Bonus: Working with NgRx in our Project | 378. Getting Started with NgRx for Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,17 +1,22 @@
-import { AuthService } from './../auth/auth.service';
-import { Ingredient } from './ingredient.model';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { map, tap, } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   private httpUrl: string =  'https://ng-course-project-2aa9a.firebaseio.com/recipes.json';
 
-  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -37,7 +42,8 @@ export class DataStorageService {
         });
       }),
       tap(recipes => {
-        this.recipeService.setRecipes(recipes);
+        // this.recipeService.setRecipes(recipes);
+        this.store.dispatch(new RecipesActions.SetRecipes(recipes));
       })
     );
   }
